df37056a27620a3262f1ade1d5131cfca453cd5c
TryIO <io.trydent@gmail.com>
2018-05-16 14:35:32 +0200

    first commit

---
+++ src/main/java/io/jug/twoface/Drink.java
@@ -0,0 +1,59 @@
+package io.jug.twoface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Drink implements Comparable<Drink> {
+  private Map<String, Integer> recipe = new HashMap<String, Integer>();//map ingredients to units per
+  private String name;
+  private double totalCost = 0;
+  private boolean makeable = false;
+
+  public Drink(String name, String[] recipe) {
+    this.name = name;
+    setRecipe(recipe);
+  }
+
+  public int compareTo(Drink drink) {
+    return name.compareTo(drink.getName());
+  }
+
+  public Map<String, Integer> getRecipe() {
+    return recipe;
+  }
+
+  public void setRecipe(String[] recipe) {
+    for (String s : recipe) {
+      if (this.recipe.containsKey(s)) {
+        this.recipe.put(s, this.recipe.get(s) + 1);//increment if multiple units
+      } else {
+        this.recipe.put(s, 1);//insert first occurrence of ingredient
+      }
+    }
+  }
+
+  public double getCost() {
+    return totalCost;
+  }
+
+  public void setCost(double totalCost) {
+    this.totalCost = totalCost;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean getMakeable() {
+    return makeable;
+  }
+
+  public void setMakeable(boolean makeable) {
+    this.makeable = makeable;
+  }
+
+}
